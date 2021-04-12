ce8794e8dfc0fe3d1c8ad30a9cb27f2383bab9e4
Kody Deda <kodydeda4@gmail.com>
2021-04-12 10:05:11 -0400

    moved sql action into its own file with template for actions

--- Introspect/ContentView.swift
+++ Introspect/ContentView.swift
@@ -7,42 +7,7 @@
 
 import SwiftUI
 
-enum SQLAction: String, Identifiable, CaseIterable {
-    var id: SQLAction { self }
-    
-    case createUsersTable = "Create users Table"
-    case createPostsTable = "Create posts Table"
-    case addAliceUser     = "Insert alice into users Table"
-    
-    var code: String {
-        switch self {
-        
-        case .createUsersTable:
-            return """
-            CREATE TABLE users (
-                id INTEGER PRIMARY KEY NOT NULL,
-                email TEXT UNIQUE NOT NULL,
-                name TEXT
-            );
-            """
-        case .createPostsTable:
-            return """
-            CREATE TABLE posts (
-                id INTEGER PRIMARY KEY NOT NULL,
-                title TEXT NOT NULL,
-                body TEXT NOT NULL,
-                published_at DATETIME
-            );
-            """
-        case .addAliceUser:
-            return """
-            INSERT INTO users (email) VALUES ('alice@mac.com')
-            """
-        }
-    }
-    
-    
-}
+
 
 struct ContentView: View {
     let f = MySQLStripped.init()

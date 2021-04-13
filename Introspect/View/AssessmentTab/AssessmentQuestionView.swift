2a1fa16796f758ba38e4f38743d6a537a3f2a6c6
Kody Deda <kodydeda4@gmail.com>
2021-04-13 07:30:23 -0400

    created buttons view

--- Introspect/View/AssessmentTab/AssessmentQuestionView.swift
+++ Introspect/View/AssessmentTab/AssessmentQuestionView.swift
@@ -13,62 +13,33 @@ struct AssessmentQuestionView: View {
     
     var body: some View {
         WithViewStore(store) { viewStore in
-            GeometryReader { geo in
-                VStack(alignment: .leading) {
-                    Progressbar(percentage: viewStore.percentCompleted, action: { viewStore.send(.showSheetView) })
-                    
-                    // Question
-                    Text(viewStore.currentQuestion.content)
-                        .font(.title)
-                        .bold()
-                        .padding(.vertical)
-                        //.frame(height: geo.size.height * 0.25)
-                        .frame(height: geo.size.height * 0.3, alignment: .topLeading)
-                    
-                    
-                    DebugView(store: store)
-                    
-                    //Spacer()
-                    HStack {
-                        Spacer()
-                        //                        Text("Agree")
-                        //                            .opacity((viewStore.changingQuestion && ![.stronglyAgree, .somewhatAgree, .agree].contains(viewStore.currentQuestion.response) ? 0.1 : 1))
-                        //                            .animation(.default, value: [.stronglyAgree].contains(viewStore.currentQuestion.response))
-                        
-                        ForEach(Question.Response.allCases) { response in
-                            Button(action: { viewStore.send(.responseButtonTapped(response)) }) {
-                                Circle()
-                                    .foregroundColor(response.buttonColor)
-                                
-                            }
-                            .opacity((viewStore.changingQuestion && viewStore.currentQuestion.response != response) ? 0.25 : 1)
-                            .animation(.default, value: viewStore.changingQuestion && viewStore.currentQuestion.response != response)
-                            //                            .frame(width: 25)
-                            
-                        }
-                        //                        Text("Disagree")
-                        //                            .opacity((viewStore.changingQuestion && ![.stronglyDisagree, .somewhatDisagree, .disagree].contains(viewStore.currentQuestion.response) ? 0.1 : 1))
-                        //                            .animation(.spring(), value: [.stronglyAgree].contains(viewStore.currentQuestion.response))
-                        Spacer()
-                    }
-                    .frame(height: geo.size.height * 0.25)
-                    
-                    //                HStack {
-                    //                    Button("Back") {
-                    //                        viewStore.send(.backButtonTapped)
-                    //                    }
-                    //                    .buttonStyle(RoundedRectangleButtonStyle())
-                    //                    .disabled(viewStore.progress == .firstQuestion)
-                    //                    .opacity(viewStore.progress == .firstQuestion ? 0.60 : 1.0)
-                    //
-                    //                    Button(viewStore.progress == .lastQuestion ? "Finish" : "Next") {
-                    //                        viewStore.send(.nextButtonTapped)
-                    //                    }
-                    //                    .buttonStyle(RoundedRectangleButtonStyle(color: .accentColor))
-                    //                }
-                    //                .padding(.vertical)
-                    //                .disabled(viewStore.changingQuestion)
-                }
+            VStack(alignment: .leading) {
+                Progressbar(percentage: viewStore.percentCompleted, action: { viewStore.send(.showSheetView) })
+                
+                Text(viewStore.currentQuestion.content)
+                    .font(.title)
+                    .bold()
+                    .padding(.vertical)
+                    .frame(height: 300, alignment: .topLeading)
+                
+                ButtonsView(store: store)
+                DebugView(store: store)
+                
+                //                HStack {
+                //                    Button("Back") {
+                //                        viewStore.send(.backButtonTapped)
+                //                    }
+                //                    .buttonStyle(RoundedRectangleButtonStyle())
+                //                    .disabled(viewStore.progress == .firstQuestion)
+                //                    .opacity(viewStore.progress == .firstQuestion ? 0.60 : 1.0)
+                //
+                //                    Button(viewStore.progress == .lastQuestion ? "Finish" : "Next") {
+                //                        viewStore.send(.nextButtonTapped)
+                //                    }
+                //                    .buttonStyle(RoundedRectangleButtonStyle(color: .accentColor))
+                //                }
+                //                .padding(.vertical)
+                //                .disabled(viewStore.changingQuestion)
                 
             }
             .padding()
@@ -85,6 +56,45 @@ struct AssessmentQuestionView_Previews: PreviewProvider {
         AssessmentQuestionView(store: Assessment.defaultStore)
     }
 }
+
+private struct ButtonsView: View {
+    let store: Store<Assessment.State, Assessment.Action>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            
+            HStack {
+                Spacer()
+                //                        Text("Agree")
+                //                            .opacity((viewStore.changingQuestion && ![.stronglyAgree, .somewhatAgree, .agree].contains(viewStore.currentQuestion.response) ? 0.1 : 1))
+                //                            .animation(.default, value: [.stronglyAgree].contains(viewStore.currentQuestion.response))
+                
+                ForEach(Question.Response.allCases) { response in
+                    Button(action: { viewStore.send(.responseButtonTapped(response)) }) {
+                        Circle()
+                            .foregroundColor(response.buttonColor)
+                        
+                    }
+                    .opacity((viewStore.changingQuestion && viewStore.currentQuestion.response != response) ? 0.25 : 1)
+                    .animation(.default, value: viewStore.changingQuestion && viewStore.currentQuestion.response != response)
+                    //                            .frame(width: 25)
+                    
+                }
+                //                        Text("Disagree")
+                //                            .opacity((viewStore.changingQuestion && ![.stronglyDisagree, .somewhatDisagree, .disagree].contains(viewStore.currentQuestion.response) ? 0.1 : 1))
+                //                            .animation(.spring(), value: [.stronglyAgree].contains(viewStore.currentQuestion.response))
+                Spacer()
+            }
+        }
+    }
+}
+
+struct ButtonsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonsView(store: Assessment.defaultStore)
+    }
+}
+
 
 // MARK:- DebugView
 private struct DebugView: View {

cb59b8f03e96a153704060803fec0c22af276fa0
Kody Deda <kodydeda4@gmail.com>
2021-04-15 08:25:07 -0400

    simplified PersonalityTypesView

--- Introspect/View/PersonalityTypesTab/PersonalityTypesView.swift
+++ Introspect/View/PersonalityTypesTab/PersonalityTypesView.swift
@@ -14,64 +14,66 @@ struct PersonalityTypesView: View {
     var body: some View {
         WithViewStore(store) { viewStore in
             ScrollView {
-                VStack(alignment: .leading) {
-                    ForEach(PersonalityType.Group.allCases) { group in
-                        Divider()
-                            .padding(.horizontal)
-                        
-                        Text(group.rawValue)
-                            .font(.title)
-                            .bold()
-                            .padding(.horizontal)
-                        
-                        ScrollView(.horizontal) {
-                            HStack {
-                                ForEach(PersonalityType.allCases.filter { $0.group == group }) { type in
-                                    NavigationLink(destination: PersonalityTypeDetailView(type: type)) {
-                                        ZStack {
-                                            type.group.associatedColor
-                                                .clipShape(RoundedRectangle(cornerRadius: 20))
-                                                .padding(.leading)
-                                            
-                                            Image(type.name.lowercased())
-                                                .resizable()
-                                                .scaledToFit()
-                                            
-                                            VStack(alignment: .leading) {
-                                                Spacer()
-                                                HStack {
-                                                    VStack(alignment: .leading) {
-                                                        Text(type.rawValue)
-                                                            .font(.caption)
-                                                            .foregroundColor(.secondary)
-                                                        
-                                                        Text(type.name)
-                                                            .bold()
-                                                            .foregroundColor(.primary)
-                                                    }
-                                                    Spacer()
-                                                }
-                                                .padding(6)
-                                                .padding([.leading, .bottom], 6)
-                                                .frame(maxWidth: .infinity)
-                                                .background(Color(.secondarySystemBackground))
-                                            }
+                ForEach(PersonalityType.Group.allCases) { group in
+                    Divider()
+                        .padding(.horizontal)
+                    
+                    Text(group.rawValue)
+                        .font(.title)
+                        .bold()
+                        .padding(.horizontal)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            ForEach(PersonalityType.allCases.filter { $0.group == group }) { type in
+                                NavigationLink(destination: PersonalityTypeDetailView(type: type)) {
+                                    ZStack {
+                                        type.group.associatedColor
                                             .clipShape(RoundedRectangle(cornerRadius: 20))
                                             .padding(.leading)
-                                        }
                                         
-                                        .padding(.vertical)
-                                        .frame(height: 225)
+                                        Image(type.name.lowercased())
+                                            .resizable()
+                                            .scaledToFit()
+                                        
+                                        VStack(alignment: .leading) {
+                                            Spacer()
+                                            HStack {
+                                                VStack(alignment: .leading) {
+                                                    Text(type.rawValue)
+                                                        .font(.caption)
+                                                        .foregroundColor(.secondary)
+                                                    
+                                                    Text(type.name)
+                                                        .bold()
+                                                        .foregroundColor(.primary)
+                                                }
+                                                Spacer()
+                                            }
+                                            .padding(6)
+                                            .padding([.leading, .bottom], 6)
+                                            .frame(maxWidth: .infinity)
+                                            .background(Color(.secondarySystemBackground))
+                                        }
+                                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                                        .padding(.leading)
                                     }
+                                    
+                                    .padding(.vertical)
+                                    .frame(height: 225)
                                 }
                             }
                         }
-                        Text(group.description)
-                            .font(.body)
-                            .foregroundColor(.secondary)
-                            .padding(.horizontal)
                     }
+                    Text(group.description)
+                        .font(.body)
+                        .foregroundColor(.secondary)
+                        .padding(.horizontal)
+                    
                 }
+                Spacer()
+                    .frame(height: 50)
             }
             .navigationTitle("Personality Types")
             .toolbar {
